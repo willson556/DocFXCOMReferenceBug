7dcde59fc2a2e0c04beb3a79d65af39a987fbbd5
Thomas Willson <willsont@curtisinst.com>
2018-12-17 11:10:45 -0800

    Initial Import

---
+++ ReferencedApp/AClass.cs
@@ -0,0 +1,14 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AClass.cs" company="Curtis Instruments">
+// Copyright (c) Curtis Instruments. All rights reserved.
+// </copyright>
+// <author>willsont</author>
+// -----------------------------------------------------------------------
+
+namespace ReferencedApp
+{
+    public static class AClass
+    {
+        public static void DoNothing() { }
+    }
+}
